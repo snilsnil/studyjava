8f005879ab4dcf29a7ce37426e5fab701a41cd2a
snilsnil <heo6601@naver.com>
2023-01-15 18:35:23 +0900

    자바공부

---
+++ src/hello1.java
@@ -0,0 +1,17 @@
+public class hello1 {
+    public static int sum(int n, int m) {
+        return n + m;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int i = 20;
+        int s;
+        char a;
+
+        s = sum(i, 10);
+        a = '?';
+        System.out.println(a);
+        System.out.println("hello");
+        System.out.println(s);
+    }
+}
