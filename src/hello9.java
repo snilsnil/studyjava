c57b0094a970b1bd37b0d847cde30a21ef17d897
snilsnil <heo6601@naver.com>
2023-01-16 17:52:13 +0900

    2일차

---
+++ src/hello9.java
@@ -0,0 +1,9 @@
+//2-8 예제
+
+public class hello9 {
+    public static void main(String[] args) {
+        int a = 3, b = 5;
+
+        System.out.println(" 두 수의 차는 " + ((a > b) ? (a - b) : (b - a)));
+    }
+}
